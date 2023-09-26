dc03dd94c1725b126e529be4fbd394b93c909b3e
remidio2015 <remidio2015@outlook.com.br>
2023-09-26 20:54:02 -0300

    Mobile

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ComponentTest} from './Componentes'
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <ComponentTest/>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,3 +20,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
---
+++ Componentes.jsx
@@ -0,0 +1,9 @@
+import { styleSheet, Text, View } from 'react-native';
+
+export function Test() {
+    return (
+<Text>
+Ronaldo
+</Text>
+    );
+}
